9aadd18bda0b962d4fe4f9ea41aedc2d97fffa78
NanoByte <marseniym@gmail.com>
2018-04-23 12:54:16 +0300

    Initial commit

---
+++ src/com/company/FedTimetable.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+public class FedTimetable {
+    public void create() {
+        JFrame frame = new JFrame("FedTimetable");
+        frame.setLocation(300, 300);
+        frame.setSize(600, 600);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ src/com/company/PoyasTimetable.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.swing.*;
+
+public class PoyasTimetable {
+    public void create(JFrame frame0) {
+        JFrame frame = new JFrame("FedTimetable");
+        frame.setLocation(600, 300);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
