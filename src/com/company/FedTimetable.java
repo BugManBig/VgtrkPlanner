5b5c23c481c951515d7a78cd6ef1172f231ca99e
NanoByte <marseniym@gmail.com>
2018-04-23 17:26:56 +0300

    Create window design done

--- src/com/company/FedTimetable.java
+++ src/com/company/FedTimetable.java
@@ -3,9 +3,6 @@ package com.company;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.concurrent.TimeUnit;
 
 public class FedTimetable {
     private static final int FRAME_WIDTH = 600;
@@ -13,7 +10,7 @@ public class FedTimetable {
 
     public void create() {
         JFrame frame = new JFrame("FedTimetable");
-        frame.setLocation(300, 300);
+        frame.setLocation(500, 200);
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -21,7 +18,7 @@ public class FedTimetable {
 
         JList<String> list = new JList<>();
         JScrollPane scrollPane = new JScrollPane(list);
-        scrollPane.setBounds(10, 10, FRAME_WIDTH - 40, 100);
+        scrollPane.setBounds(10, 10, FRAME_WIDTH - 40, 500);
         frame.add(scrollPane);
 
         JButton buttonForCreate = new JButton("Create");
@@ -41,38 +38,93 @@ public class FedTimetable {
     private void createFrameForNewElement() {
         JFrame frame = new JFrame("Create element");
         frame.setSize(400, 200);
-        frame.setLocation(200, 200);
+        frame.setLocation(600, 400);
         frame.setVisible(true);
         frame.setLayout(null);
 
+
+        JLabel timeLabel = new JLabel("Time:");
+        timeLabel.setBounds(10, 10, 50, 25);
+        frame.add(timeLabel);
+
         JTextField hoursText = new JTextField();
-        hoursText.setBounds(10, 10, 25, 25);
+        hoursText.setBounds(60, 10, 25, 25);
         frame.add(hoursText);
 
         JTextField minutesText = new JTextField();
-        minutesText.setBounds(40, 10, 25, 25);
+        minutesText.setBounds(90, 10, 25, 25);
         frame.add(minutesText);
 
-        hoursText.addKeyListener(new KeyListener() {
+        JTextField secondsText = new JTextField();
+        secondsText.setBounds(120, 10, 25, 25);
+        frame.add(secondsText);
+
+
+        JLabel chronoLabel = new JLabel("Chrono:");
+        chronoLabel.setBounds(230, 10, 50, 25);
+        frame.add(chronoLabel);
+
+        JTextField chronoHoursText = new JTextField();
+        chronoHoursText.setBounds(290, 10, 25, 25);
+        frame.add(chronoHoursText);
+
+        JTextField chronoMinutesText = new JTextField();
+        chronoMinutesText.setBounds(320, 10, 25, 25);
+        frame.add(chronoMinutesText);
+
+        JTextField chronoSecondsText = new JTextField();
+        chronoSecondsText.setBounds(350, 10, 25, 25);
+        frame.add(chronoSecondsText);
+
+
+        JLabel nameLabel = new JLabel("Name:");
+        nameLabel.setBounds(10, 50, 50, 25);
+        frame.add(nameLabel);
+
+        JTextField nameTextfield = new JTextField();
+        nameTextfield.setBounds(60, 50, 250, 25);
+        frame.add(nameTextfield);
+
+        hoursText.addKeyListener(new KeyImpl(hoursText, minutesText));
+        minutesText.addKeyListener(new KeyImpl(minutesText, secondsText));
+        secondsText.addKeyListener(new KeyImpl(secondsText, chronoHoursText));
+        chronoHoursText.addKeyListener(new KeyImpl(chronoHoursText, chronoMinutesText));
+        chronoMinutesText.addKeyListener(new KeyImpl(chronoMinutesText, chronoSecondsText));
+        chronoSecondsText.addKeyListener(new KeyImpl(chronoSecondsText, nameTextfield));
+
+        addCheckboxes(frame);
+
+        JButton buttonOk = new JButton("Create");
+        buttonOk.setBounds(10, 120, 100, 30);
+        buttonOk.addActionListener(new ActionListener() {
             @Override
-            public void keyTyped(KeyEvent e) {
+            public void actionPerformed(ActionEvent e) {
 
-            }
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (hoursText.getText().length() == 2) {
-                    minutesText.requestFocus();
-                }
             }
         });
+        frame.add(buttonOk);
+
+        JButton buttonCancel = new JButton("Cancel");
+        buttonCancel.setBounds(130, 120, 100, 30);
+        buttonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+        frame.add(buttonCancel);
 
         frame.repaint();
         frame.revalidate();
+    }
+
+    private void addCheckboxes(JFrame frame) {
+        String[] days = {"Mon", "Tue", "Wen", "Thu", "Fri", "Sat", "Sun"};
+        for (int i = 0; i < 7; i++) {
+            JCheckBox checkBox = new JCheckBox();
+            checkBox.setText(days[i]);
+            checkBox.setBounds(10 + i * 50, 80, 52, 25);
+            frame.add(checkBox);
+        }
     }
 }
---
+++ src/com/company/KeyImpl.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyImpl implements KeyListener {
+    private JTextField from;
+    private JTextField to;
+
+    public KeyImpl(JTextField from, JTextField to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (from.getText().length() == 2) {
+            to.requestFocus();
+        }
+    }
+}
